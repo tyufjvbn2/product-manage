bb7d4822b28d4c80d5cb45bfd38b3aadd2501b1e
tyufjvbn2 <tyufjvbn22@gmail.com>
2025-02-21 05:20:37 +0900

    [build] first project set

---
+++ src/main/kotlin/org/tyufjvbn2/productmanage/ProductManageApplication.kt
@@ -0,0 +1,11 @@
+package org.tyufjvbn2.productmanage
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProductManageApplication
+
+fun main(args: Array<String>) {
+    runApplication<ProductManageApplication>(*args)
+}
---
+++ src/test/kotlin/org/tyufjvbn2/productmanage/ProductManageApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.tyufjvbn2.productmanage
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProductManageApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
